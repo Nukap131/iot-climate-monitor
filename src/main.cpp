f43c14823abeac424c77c9d4f1fc10ce0d9a7569
Nukap131 <nukaaqqalu@gmail.com>
2026-06-06 20:27:38 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,10 @@ const char* ssid = "MomsSpaghetti";
 const char* password = "Bustu313";
 
 // ---------- MQTT ----------
-const char* mqtt_server = "192.168.197.229"; // din Debian IP
+const char* mqtt_server = "192.168.197.229";
 const int mqtt_port = 1883;
 const char* mqtt_topic = "lager/teknikrum/sensorer";
+const char* mqtt_control_topic = "lager/teknikrum/control";
 
 // ---------- SENSOR ----------
 #define DHTPIN 4
@@ -29,6 +30,9 @@ PubSubClient client(espClient);
 const float TEMP_LIMIT = 25.0;
 const int GAS_LIMIT = 400;
 
+// ---------- SYSTEM STATE ----------
+bool systemEnabled = true;
+
 // ---------- MOTOR ----------
 void motorStart() {
   digitalWrite(MOTOR_IN1, HIGH);
@@ -38,6 +42,29 @@ void motorStart() {
 void motorStop() {
   digitalWrite(MOTOR_IN1, LOW);
   digitalWrite(MOTOR_IN2, LOW);
+}
+
+// ---------- MQTT CALLBACK ----------
+void callback(char* topic, byte* payload, unsigned int length) {
+  String message;
+
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+
+  Serial.print("MQTT command received: ");
+  Serial.println(message);
+
+  if (message == "OFF") {
+    systemEnabled = false;
+    motorStop();
+    Serial.println("System disabled");
+  }
+
+  if (message == "ON") {
+    systemEnabled = true;
+    Serial.println("System enabled");
+  }
 }
 
 // ---------- WIFI ----------
@@ -62,9 +89,11 @@ void reconnect() {
     Serial.print("Connecting to MQTT...");
     if (client.connect("ESP32Client")) {
       Serial.println("connected");
+      client.subscribe(mqtt_control_topic);
+      Serial.println("Subscribed to control topic");
     } else {
       Serial.print("failed, rc=");
-      Serial.print(client.state());
+      Serial.println(client.state());
       delay(2000);
     }
   }
@@ -81,22 +110,48 @@ void setup() {
   motorStop();
 
   setup_wifi();
+
   client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 
   Serial.println("System starter...");
 }
 
 // ---------- LOOP ----------
 void loop() {
-
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
+  if (!systemEnabled) {
+    motorStop();
+
+    String payload = "{";
+    payload += "\"temperature\":0,";
+    payload += "\"humidity\":0,";
+    payload += "\"gas\":0,";
+    payload += "\"motor\":0,";
+    payload += "\"alarm\":0,";
+    payload += "\"system\":\"OFF\"";
+    payload += "}";
+
+    Serial.println(payload);
+    client.publish(mqtt_topic, payload.c_str());
+
+    delay(5000);
+    return;
+  }
+
   float temp = dht.readTemperature();
   float hum = dht.readHumidity();
   int gasValue = analogRead(GAS_PIN);
+
+  if (isnan(temp) || isnan(hum)) {
+    Serial.println("DHT sensor error");
+    delay(2000);
+    return;
+  }
 
   bool highTemp = temp > TEMP_LIMIT;
   bool gasDetected = gasValue > GAS_LIMIT;
@@ -112,17 +167,16 @@ void loop() {
     motorStop();
   }
 
-  // ---------- JSON ----------
   String payload = "{";
-  payload += "\"temperature\":" + String(temp) + ",";
-  payload += "\"humidity\":" + String(hum) + ",";
+  payload += "\"temperature\":" + String(temp, 1) + ",";
+  payload += "\"humidity\":" + String(hum, 1) + ",";
   payload += "\"gas\":" + String(gasValue) + ",";
   payload += "\"motor\":" + String(motorState) + ",";
-  payload += "\"alarm\":" + String(alarm);
+  payload += "\"alarm\":" + String(alarm) + ",";
+  payload += "\"system\":\"ON\"";
   payload += "}";
 
   Serial.println(payload);
-
   client.publish(mqtt_topic, payload.c_str());
 
   delay(5000);
